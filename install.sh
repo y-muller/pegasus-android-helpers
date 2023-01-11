0cce3ca6f4fe5038f2e9e5ef515ecd856d9b44fc
Yann Muller <yann@darkriver.net>
2023-01-11 20:45:36 +0000

    test 12

--- install.sh
+++ install.sh
@@ -12,17 +12,17 @@ cmd package list packages|grep com.termux
 cmd package list packages|grep com.llamalab.automate
 if [[ $(cmd package list packages|grep com.termux.api) == "" ]]; then
     echo Termux:API is not installed
-    termux-open https://f-droid.org/packages/com.termux.api
+    #termux-open https://f-droid.org/packages/com.termux.api
     read -n 1 -r -s -p "Press any key to continue..." key
 fi
 if [[ $(cmd package list packages|grep com.termux.widget) == "" ]]; then
     echo Termux:Widget is not installed
-    termux-open https://f-droid.org/packages/com.termux.widget
+    #termux-open https://f-droid.org/packages/com.termux.widget
     read -n 1 -r -s -p "Press any key to continue..." key
 fi
 if [[ $(cmd package list packages|grep com.llamalab.automate) == "" ]]; then
     echo Automate is not installed
-    termux-open https://play.google.com/store/apps/details?id=com.llamalab.automate
+    #termux-open https://play.google.com/store/apps/details?id=com.llamalab.automate
     read -n 1 -r -s -p "Press any key to continue..." key
 fi
 
