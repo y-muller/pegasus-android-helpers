ced6946eb16c0826cee86ae5d6a953059b7b1d04
Yann Muller <yann@darkriver.net>
2023-01-11 20:37:03 +0000

    test 10

--- install.sh
+++ install.sh
@@ -10,17 +10,20 @@ pkg update -y && pkg upgrade -y
 # install required app - 
 cmd package list packages|grep com.termux
 cmd package list packages|grep com.llamalab.automate
-if ! cmd package list packages|grep com.termux.api then
+if ! cmd package list packages|grep com.termux.api
+then
     echo Termux:API is not installed
     termux-open https://f-droid.org/packages/com.termux.api
     read -n 1 -r -s -p "Press any key to continue..." key
 fi
-if ! cmd package list packages|grep com.termux.widget then
+if ! cmd package list packages|grep com.termux.widget
+then
     echo Termux:Widget is not installed
     termux-open https://f-droid.org/packages/com.termux.widget
     read -n 1 -r -s -p "Press any key to continue..." key
 fi
-if ! cmd package list packages|grep com.llamalab.automate then
+if ! cmd package list packages|grep com.llamalab.automate
+then
     echo Automate is not installed
     termux-open https://play.google.com/store/apps/details?id=com.llamalab.automate
     read -n 1 -r -s -p "Press any key to continue..." key
