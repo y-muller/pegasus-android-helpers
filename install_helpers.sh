31347b77ccfd7f0a9a2b26a616af6132b978ba2c
Yann Muller <yann@darkriver.net>
2023-01-12 16:27:03 +0000

    Copy automate flow2

--- install_helpers.sh
+++ install_helpers.sh
@@ -1,7 +1,13 @@
 # copy scripts to .shortcuts
 cp scripts/* ~/.shortcuts/
+echo "Helper scripts copied to ~/.shotcuts"
 
 # copy example Skyscraper config to .skyscraper
 cp skyscraper/* ~/.skyscraper
+echo "Sample configs copied to ~/.skycraper"
 
+# copy the Automate flow to Download
+cp automate/* /storage/emulated/0/Download/
+echo "Automate flow App2Pegasus copied to Download."
+echo "  Import it in Automate or delete it if unneeded."
 
